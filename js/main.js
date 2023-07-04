90f0d3a51887927ff92230b5d9f5729db615fab8
michelepiccinno <michele.piccinno@gmail.com>
2023-07-04 15:01:09 +0200

    add-js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 100; i++) {
+  //console.log(i);
+  if (((i) % 3 === 0) && ((i) % 5 === 0)) {
+    console.log((i)+' FIZZBUZZ');
+  } else if ((i) % 5 === 0) {
+    console.log((i)+' BUZZ');
+  } else if ((i) % 3 === 0) {
+    console.log((i)+' FIZZ');
+  }
+}
